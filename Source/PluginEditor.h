bebce9550eea83999c4d8f82b68e42db765bb185
PrajyotGajane <prajyotgajane@gmail.com>
2021-07-05 00:02:17 +0530

    Pre rendered grid into background, Added grid lines into

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -69,6 +69,8 @@ struct ResponseCurveComponent : juce::Component,
     void timerCallback() override;
 
     void paint(juce::Graphics& g) override;
+
+    void resized() override;
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parameterChanged{ false };
@@ -76,7 +78,14 @@ private:
     MonoChain monoChain;
 
     void updateChain();
+
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+
+    juce::Rectangle<int> getAnalysisArea();
 };
+
 
 class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
